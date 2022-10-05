0d19e8595a86cf56eea20ac7abaeaa7e2aa1ed22
Sadeq Mozaffari <sadeqmozaffari@gmail.com>
2022-10-05 21:31:59 +0430

    init admin panel

---
+++ src/data/dummy.js
@@ -0,0 +1,116 @@
+import React from "react";
+import { BsCurrencyDollar, BsShield } from "react-icons/bs";
+
+import { FiCreditCard, FiShoppingBag } from "react-icons/fi";
+import avatar from "./avatar.jpg";
+import avatar2 from "./avatar2.jpg";
+import avatar3 from "./avatar3.png";
+import avatar4 from "./avatar4.jpg";
+
+export const cartData = [
+  {
+    image: "",
+    name: "butterscotch ice-cream",
+    category: "Milk product",
+    price: "$250",
+  },
+  {
+    image: "product6",
+    name: "Supreme fresh tomato",
+    category: "Vegetable Item",
+    price: "$450",
+  },
+  {
+    image: "product7",
+    name: "Red color candy",
+    category: "Food Item",
+    price: "$190",
+  },
+];
+
+export const chatData = [
+  {
+    image: avatar2,
+    message: "Roman Joined the Team!",
+    desc: "Congratulate him",
+    time: "9:08 AM",
+  },
+  {
+    image: avatar3,
+    message: "New message received",
+    desc: "Salma sent you new message",
+    time: "11:56 AM",
+  },
+  {
+    image: avatar4,
+    message: "New Payment received",
+    desc: "Check your earnings",
+    time: "4:39 AM",
+  },
+  {
+    image: avatar,
+    message: "Jolly completed tasks",
+    desc: "Assign her new tasks",
+    time: "1:12 AM",
+  },
+];
+export const links = [
+  {
+    title: "Dashboard",
+    links: [
+      {
+        name: "ecommerce",
+        icon: <FiShoppingBag />,
+      },
+    ],
+  },
+];
+export const themeColors = [
+  {
+    name: "blue-theme",
+    color: "#1A97F5",
+  },
+  {
+    name: "green-theme",
+    color: "#03C9D7",
+  },
+  {
+    name: "purple-theme",
+    color: "#7352FF",
+  },
+  {
+    name: "red-theme",
+    color: "#FF5C8E",
+  },
+  {
+    name: "indigo-theme",
+    color: "#1E4DB7",
+  },
+  {
+    color: "#FB9678",
+    name: "orange-theme",
+  },
+];
+export const userProfileData = [
+  {
+    icon: <BsCurrencyDollar />,
+    title: "My Profile",
+    desc: "Account Settings",
+    iconColor: "#03C9D7",
+    iconBg: "#E5FAFB",
+  },
+  {
+    icon: <BsShield />,
+    title: "My Inbox",
+    desc: "Messages & Emails",
+    iconColor: "rgb(0, 194, 146)",
+    iconBg: "rgb(235, 250, 242)",
+  },
+  {
+    icon: <FiCreditCard />,
+    title: "My Tasks",
+    desc: "To-do and Daily Tasks",
+    iconColor: "rgb(255, 244, 229)",
+    iconBg: "rgb(254, 201, 15)",
+  },
+];
---
+++ src/pages/Ecommerce.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Ecommerce = () => {
+  return (
+    <div className="mt-24">
+      <div className="flex flex-wrap lg:flex-nowrap justify-center ">
+        Ecommerce
+      </div>
+    </div>
+  );
+};
+
+export default Ecommerce;
